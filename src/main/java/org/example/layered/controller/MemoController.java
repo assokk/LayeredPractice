e85401acfacaf1691dfcee458f093e648cd22a94
assokk <mijisuu@naver.com>
2025-08-08 18:23:42 +0900

    refactor: 메모 단건 전체 수정 API

--- src/main/java/org/example/layered/controller/MemoController.java
+++ src/main/java/org/example/layered/controller/MemoController.java
@@ -57,4 +57,12 @@ import java.util.Map;
             return new ResponseEntity<>(memoService.findMemoById(id), HttpStatus.OK);
         }
 
+        @PutMapping("/{id}")
+        public ResponseEntity<MemoResponseDto> updateMemo(
+                @PathVariable Long id,
+                @RequestBody MemoRequestDto dto) {
+
+            return new ResponseEntity<>(memoService.updateMemo(id, dto.getTitle(), dto.getContents()), HttpStatus.OK);
+        }
+
     }
